99980d86fb54aa5cd1f6a4cbd54a4a436c2693a2
APCSP <APCSP@jh-mba158-30.d214.org>
2017-10-11 08:57:32 -0500

    Initial Commit

---
+++ React/ArrayOf.swift
@@ -0,0 +1,13 @@
+//
+//  ArrayOf.swift
+//  React
+//
+//  Created by APCSP on 10/11/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+class ArrayOf {
+    var scores:[Double] = []
+}
---
+++ React/TableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewController.swift
+//  React
+//
+//  Created by APCSP on 10/11/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrayOf = ArrayOf()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return arrayOf.scores.count
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+
+}
